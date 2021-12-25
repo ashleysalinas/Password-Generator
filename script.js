d66353e66fc24236e48b29624c9ce79560b761b2
ashleysalinas <ashleysalinas98@protonmail.com>
2021-12-25 10:05:30 -0600

    master

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  //initial pop up
+  var initialPopUp = window.prompt("Thank you for using Password Generator! How many characters would you like to include? Please choose between 8-128.");
+  //if/else statement to control character length
+  if (initialPopUp == null) {
+    return window.alert("Thanks again! Have a great day.");
+  } else if (initialPopUp < 8 || initialPopUp > 128) {
+    return window.alert("Please enter a number between 8 and 128. Try again!");
+  } else {
+    return window.alert("It's working!")
+  }
+  
 
+  console.log(initialPopUp);
+
+  //var includeLowerCase = window.alert("Would you like to include lower case characters?")
   passwordText.value = password;
 
 }
@@ -13,4 +28,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// test test test
