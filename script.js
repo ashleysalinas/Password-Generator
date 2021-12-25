4f22b4631db7b4d32d7d4630535714c34ed477fd
ashleysalinas <ashleysalinas98@protonmail.com>
2021-12-25 10:42:14 -0600

    master

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var lowercaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 // Write password to the #password input
 function writePassword() {
-  //var password = generatePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  //initial pop up
-  var initialPopUp = window.prompt("Thank you for using Password Generator! How many characters would you like to include? Please choose between 8-128.");
-  //if/else statement to control character length
-  if (initialPopUp == null) {
-    return window.alert("Thanks again! Have a great day.");
-  } else if (initialPopUp < 8 || initialPopUp > 128) {
-    return window.alert("Please enter a number between 8 and 128. Try again!");
-  } else {
-    return window.alert("It's working!")
-  }
-  
 
-  console.log(initialPopUp);
-
-  //var includeLowerCase = window.alert("Would you like to include lower case characters?")
   passwordText.value = password;
 
 }
 
+function generatePassword() {
+  // Initial prompt for choosing character length
+  var initialPopUp = window.prompt("Thank you for using the Password Generator! How many characters would you like to include? Please choose between 8-128");
+  if (initialPopUp == null || initialPopUp.some(lowercaseAlphabet)){
+    return window.alert("Please choose a number. Try again!");
+  } else if (initialPopUp < 8 || initialPopUp > 128) {
+    return window.alert("Please choose a number between 8 and 128.");
+  }
+  console.log(initialPopUp)
+  console.log(typeof initialPopUp)
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
